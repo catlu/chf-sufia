46bcf383d911c3ccf6928095a5a2bbb6c7d65973
Anna Headley <anna3LC@gmail.com>
2015-07-24 13:37:15 -0400

    Add form elements for date fields

--- app/forms/generic_file_edit_form.rb
+++ app/forms/generic_file_edit_form.rb
@@ -7,4 +7,28 @@ class GenericFileEditForm < GenericFilePresenter
 
   self.required_fields = [:title, :identifier]
 
+  protected
+
+    # Override HydraEditor::Form to treat nested attbriutes accordingly
+    def initialize_field(key)
+      if reflection = model_class.reflect_on_association(key)
+        raise ArgumentError, "Association ''#{key}'' is not a collection" unless reflection.collection?
+        build_association(key)
+      else
+        super
+      end
+    end
+
+  private
+
+    def build_association(key)
+      association = model.send(key)
+      if association.empty?
+        self[key] = Array(association.build)
+      else
+        association.build
+        self[key] = association
+      end
+    end
+
 end
--- app/forms/nested_dates.rb
+++ app/forms/nested_dates.rb
@@ -11,15 +11,19 @@ module NestedDates
 
     def permitted_time_span_params
       [ :id, :_destroy, :start, :start_qualifier, :finish, :finish_qualifier, :label, :note ]
+      #tests break when I use this nested structure which I see in other code bases.
+      #[ :id, :_destroy, {
+      #  :start => nil, :start_qualifier => nil, :finish => nil, :finish_qualifier => nil, :label => nil, :note => nil
+      #}]
     end
 
   end
 
-  def date_of_work_attributes= attributes
-    model.date_of_work_attributes= attributes
-  end
-  def date_of_publication_attributes= attributes
-    model.date_of_publication_attributes= attributes
-  end
+#  def date_of_work_attributes= attributes
+#    model.date_of_work_attributes= attributes
+#  end
+#  def date_of_publication_attributes= attributes
+#    model.date_of_publication_attributes= attributes
+#  end
 
 end
