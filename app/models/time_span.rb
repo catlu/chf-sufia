1897c658a75654a23ba902ee88ac82c83a961ad4
Anna Headley <anna3LC@gmail.com>
2015-08-06 10:19:30 -0400

    Add credit / license info for timespan class

--- app/models/time_span.rb
+++ app/models/time_span.rb
@@ -1,3 +1,18 @@
+# This class and related code draws heavily on
+# https://github.com/curationexperts/alexandria-v2/blob/master/app/models/time_span.rb
+#which is licensed as follows:
+#
+#This software is Copyright © 2012-2013 The Regents of the University of California. All Rights Reserved.
+#
+#Permission to copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice, this paragraph and the following three paragraphs appear in all copies.
+#
+#Permission to make commercial use of this software may be obtained by contacting:
+#
+#Technology Transfer Office 9500 Gilman Drive, Mail Code 0910 University of California La Jolla, CA 92093-0910 (858) 534-5815 invent@ucsd.edu
+#
+#This software program and documentation are copyrighted by The Regents of the University of California. The software program and documentation are supplied "as is", without any accompanying services from The Regents. The Regents does not warrant that the operation of the program will be uninterrupted or error-free. The end-user understands that the program was developed for research purposes and is advised not to rely exclusively on the program for any reason.
+#
+#IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 class TimeSpan < ActiveFedora::Base
   type ::RDF::Vocab::EDM.TimeSpan
 
@@ -63,6 +78,7 @@ class TimeSpan < ActiveFedora::Base
 
   # TODO: Validations
   #  - if start qualifier is 'decade' there should be no end or end qualifier. start should be a year ending '0'.
+  #  - if start qualifier is 'century' there should be no end or end qualifier. start should be a year ending '00'.
   #  - if start qualifier is 'undated' there should be no start, end, or end qualifier
   #  - if start qualifier is 'before' there should be no end or end qualifier
   #  - if end exists, start must exist
